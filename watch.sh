8b70cee2fca7b4654c4b4d62e5e57edfc9cf71c8
atiredturtle <guy@segev.com>
2020-06-11 17:51:41 +1000

    Fixes watch script

--- watch.sh
+++ watch.sh
@@ -6,6 +6,6 @@ echo $fileToRun
 if [ -z $fileToRun ]; then 
     echo "Usage: ./watch.sh <name_of_file>"; 
 else
-     ls | entr -c python3 ./692-top-k-frequent-words.py;
+     ls | entr -c python3 $fileToRun;
 fi
 
